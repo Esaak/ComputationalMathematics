98bfd2569b29084578eb48d5ffc58c1359624f07
Esaak <gerasimov.ia@phystech.edu>
2023-10-15 00:59:42 +0300

    add spline lab

--- src/Spline.hpp
+++ src/Spline.hpp
@@ -4,17 +4,40 @@
 #include <vector>
 #include <type_traits>
 #include <stdexcept>
+#include <tuple>
 
 /** класс для работы с трехдиагональной матрицей **/
-template<typename Type>
-class ThreeDiagonalMatrix;
+template<typename Type, std::size_t N>
+class ThreeDiagonalMatrix{
+private:
+    struct Triads {
+        Type a;
+        Type b;
+        Type c;
+    };
+
+    std::array<Triads, N> data;
+public:
+    std::size_t size() const;
+    //void change_matrix(const std::array<Type, N> &a, const std::array<Type, N> &b, const std::array<Type, N> &c);
+    Triads &operator[](std::size_t i);
+    const Triads &operator[](std::size_t i) const;
+    const Type &operator()(std::size_t i, std::size_t j) const;
+    Type &operator()(std::size_t i, std::size_t j);
+    const Triads &back() const;
+    Triads &back();
+};
+
 
 template<typename numeratorType, typename denominatorType>
 using DivisType = decltype(std::declval<numeratorType>() / std::declval<denominatorType>());
 
+//template<typename fType, typename sType>
+//using MullType = decltype(std::declval<fType>() * std::declval<sType>());
+
 /** Функция для решения методм  прогонки **/
-template<typename mType, typename cType>
-std::vector<DivisType<cType, mType>> solve( const ThreeDiagonalMatrix<mType>& matrix,
+template<typename mType, typename cType, std::size_t N>
+std::vector<DivisType<cType, mType>> solve( const ThreeDiagonalMatrix<mType, N>& matrix,
                                             const std::vector<cType>& column);
 
 
@@ -23,105 +46,180 @@ std::vector<DivisType<cType, mType>> solve( const ThreeDiagonalMatrix<mType>& ma
 * xType - тип аргумента x.
 * yType - тип значения функции y
 */
-template<typename xType, typename yType>
+template<typename xType, typename yType, std::size_t N>
 class CubicSpline {
-    /*** Какие-то поля ***/
 
+    struct spline{
+        yType a;
+        yType b;
+        yType c;
+        yType d;
+        xType x;
+    };
+    std::array<spline, N + 1> splines;
+    /*
+    std::array<yType, N> a;
+    std::array<yType, N> b;
+    std::array<yType, N> c;
+    std::array<yType, N> d;
+    std::array<xType, N> x;
+    */
 public:
-    CubicSpline( const std::vector<xType> &points,  // Значения x
-                 const std::vector<yType>& values  // значения y
-    ){
-
-    }
+    CubicSpline( const std::array<xType, N + 1> &points,  // Значения x
+                 const std::array<yType, N + 1>& values  // значения y
+    );
 
     yType interpolate(const xType& x) const noexcept;
 };
+//////////////////////////////ThreeDiagonalMatrix definition //////////////////////////////
 
+template<typename Type, std::size_t N>
+std::size_t ThreeDiagonalMatrix<Type, N>::size() const {
+    return N;
+}
 
+template<typename Type, std::size_t N>
+ThreeDiagonalMatrix<Type, N>::Triads &ThreeDiagonalMatrix<Type, N>::operator[](std::size_t i) {
+    return data[i];
+}
 
-template<typename Type>
-    class ThreeDiagonalMatrix{
-    private:
-    struct Triads {
-        Type a;
-        Type b;
-        Type c;
-    };
-
-    std::vector<Triads> data;
-    std::size_t N;
-    public:
-    std::size_t size() const {
-        return N;
+template<typename Type, std::size_t N>
+const ThreeDiagonalMatrix<Type, N>::Triads &ThreeDiagonalMatrix<Type, N>::operator[](std::size_t i) const {
+    return data[i];
+}
+/*
+template<typename Type, std::size_t N>
+void ThreeDiagonalMatrix<Type, N>::change_matrix(const std::array<Type, N> &a, const std::array<Type, N> &b, const std::array<Type, N> &c) {
+    data.reserve(a.size());
+    N = a.size();
+    for (std::size_t i = 0; i < N; i++) {
+        data[i].a = a[i];
+        data[i].b = b[i];
+        data[i].c = c[i];
     }
-
-    void change_matrix(const std::vector<Type> &a, const std::vector<Type> &b, const std::vector<Type> &c) {
-        data.reserve(a.size());
-        N = a.size();
-        for (std::size_t i = 0; i < a.size(); i++) {
-            data[i].a = a[i];
-            data[i].b = b[i];
-            data[i].c = c[i];
-        }
+}
+*/
+template<typename Type, std::size_t N>
+const Type &ThreeDiagonalMatrix<Type, N>::operator()(std::size_t i, std::size_t j) const {
+    if (j + 1 == i) {
+        return data[i].a;
+    } else if (j == i) {
+        return data[i].b;
+    } else if (j == i + 1) {
+        return data[i].c;
     }
+}
 
-    Triads &operator[](const std::size_t i) {
-        return data[i];
+template<typename Type, std::size_t N>
+Type &ThreeDiagonalMatrix<Type, N>::operator()(std::size_t i, std::size_t j) {
+    if (j + 1 == i) {
+        return data[i].a;
+    } else if (j == i) {
+        return data[i].b;
+    } else if (j == i + 1) {
+        return data[i].c;
     }
+}
+template<typename Type, std::size_t N>
+const ThreeDiagonalMatrix<Type, N>::Triads &ThreeDiagonalMatrix<Type, N>::back() const {
+    return data.back();
+}
+template<typename Type, std::size_t N>
+ThreeDiagonalMatrix<Type, N>::Triads &ThreeDiagonalMatrix<Type, N>::back(){
+    return data.back();
+}
 
-    const Triads &operator[](const std::size_t i) const {
-        return data[i];
-    }
-
-
-    const Type &operator()(const std::size_t i, const std::size_t j) const {
-        if (j + 1 == i) {
-            return data[i].a;
-        } else if (j == i) {
-            return data[i].b;
-        } else if (j == i + 1) {
-            return data[i].c;
-        }
-        throw std::invalid_argument("invalid argument");
-    }
-
-    Type &operator()(std::size_t i, std::size_t j) {
-        if (j + 1 == i) {
-            return data[i].a;
-        } else if (j == i) {
-            return data[i].b;
-        } else if (j == i + 1) {
-            return data[i].c;
-        }
-        throw std::invalid_argument("invalid argument");
-    }
-
-};
+///////////////////////////**************************//////////////////////////////
 
 
+template<typename mType, typename cType, std::size_t N>
+std::array<DivisType<cType, mType>, N> solve( const ThreeDiagonalMatrix<mType, N>& matrix,
+                                            const std::array<cType, N>& column) {
+    auto X = column;
+    decltype(X) Temp;
 
-
-template<typename mType, typename cType>
-std::vector<DivisType<cType, mType>> solve( const ThreeDiagonalMatrix<mType>& matrix,
-                                            const std::vector<cType>& column) {
-    std::vector<cType> X = column;
-    std::vector<cType> Temp(X.size());
-
-    int N = static_cast<int>(X.size()) - 1;
+    int N_1 = static_cast<int>(N) - 1;
 
     Temp[0] = matrix(0, 0 + 1) / matrix(0, 0);
     X[0] /= matrix(0, 0);
-    for (int i = 1; i < N; i++) {
+    for (int i = 1; i < N_1; i++) {
         Temp[i] = matrix(i, i + 1) / (matrix(i, i) - matrix(i, i - 1) * Temp[i - 1]);
         X[i] = (X[i] - matrix(i, i - 1) * X[i - 1]) / (matrix(i, i) - matrix(i, i - 1) * Temp[i - 1]);
     }
-    X.back() = (X.back() - matrix(N, N - 1) * X[N - 1]) / (matrix(N, N) - matrix(N, N - 1) * Temp[N - 1]);
+    X.back() = (X.back() - matrix(N_1, N_1 - 1) * X[N_1 - 1]) / (matrix(N_1, N_1) - matrix(N_1, N_1 - 1) * Temp[N_1 - 1]);
 
-    for (int i = N; i-- > 0;) {
+    for (int i = N_1; i-- > 0;) {
         X[i] -= Temp[i] * X[i + 1];
     }
     return X;
 
 }
+
+//////////////////////////////CubicSpline definition //////////////////////////////
+template<typename xType, typename yType, std::size_t N>
+CubicSpline<xType, yType, N>::CubicSpline(const std::array<xType, N + 1> &points, const std::array<yType, N + 1> &values){
+    constexpr xType diagEl = xType(2);
+    xType h_2;
+    std::array<yType, N> h;
+    std::array<xType, N> u;
+
+    std::array<xType, N-1> column;
+    ThreeDiagonalMatrix<xType, N-1>matrix;
+    splines.back().a = values.back();
+    splines.back().x = points.back();
+    splines.back().c = 0;
+    splines[0] = {0,0,0,0,0};
+    h[0] = points[1] - points[0];
+    h[1] = points[2] - points[1];
+    h_2 = h[1] + h[0];
+    u[1] = (values[2] - values[1])/h[1];
+    u[0] = (values[1] - values[0])/h[0];
+
+    matrix[0] = {0, diagEl, h[1]/h_2};
+
+    column[0] = 6 * (u[1] - u[0])/h_2;
+
+    for(std::size_t i = 0; i < N; i++){
+        splines[i].a = values[i];
+        splines[i].x = points[i];
+
+
+        if( i < 1 || i + 2 > N) continue;
+        h[i + 1] = points[i+2] - points[i+1];
+        h_2 = h[i+1] + h[i];
+        u[i + 1] = (values[i+2] - values[i + 1])/h[i+1];
+        column[i] = 6 * (u[i + 1] - u[i])/h_2;
+        matrix[i] = {h[i]/h_2, diagEl, h[i+1]/h_2};
+    }
+    h.back() = points.back() - *(points.end() -2);
+    u.back() = (values.back() - *(values.end() - 2))/h.back();
+    column.back() = 6 * (u.back() - *(u.end()-2))/(points.back() - *(points.end() - 3));
+    matrix.back() = {h[N - 2]/(points[N] - points[N-2]), diagEl, 0};
+    auto answC = solve(matrix, column);
+    for(std::size_t i = 0; i != answC.size(); i++){
+        splines[i+1].c = answC[i];
+        splines[i+1].b = (answC[i] + splines[i].c/yType(2)) * h[i + 1]/yType(3) + u[i];
+        splines[i+1].d = (answC[i] - splines[i].c) / h[i + 1];
+    }
+    splines.back().b = splines[answC.size()].c/yType(6) * h.back() + u[answC.size()];
+    splines.back().d = -(splines[answC.size()].c) / h.back();
+
+}
+
+template<typename xType, typename yType, std::size_t N>
+yType CubicSpline<xType, yType, N>::interpolate(const xType& x) const noexcept{
+    auto answIt = std::lower_bound(splines.begin(), splines.end(), x, [](auto &spline, const xType &x){return spline.x < x;});
+    if(answIt == splines.end()) --answIt;
+    auto it = std::distance(splines.begin(), answIt);
+    std::tuple spline = {splines[it].a, splines[it].b, splines[it].c, splines[it].d, splines[it].x};
+    yType result = std::get<0>(spline) - std::get<0>(spline);
+    result = (result + std::get<3>(spline)) * (x - std::get<4>(spline))/yType(3.);
+    result = (result + std::get<2>(spline)) * (x - std::get<4>(spline))/yType(2.);
+    result = (result + std::get<1>(spline)) * (x - std::get<4>(spline));
+    result +=std::get<0>(spline);
+    return result;
+}
+
+///////////////////////////**************************//////////////////////////////
 
 #endif //COMPMATH_SPLINE_HPP
---
+++ tests/SplineTests.cpp
@@ -0,0 +1,167 @@
+#include <gtest/gtest.h>
+#include "../src/Spline.hpp"
+#include <fstream>
+#include <string>
+#include <random>
+#include <iostream>
+#include <algorithm>
+
+const std::string py_path = "/home/esaak/my_folder/CProjects/ComputationalMathematics/py";
+TEST(SplineTests, FirstBaseTest){
+    std::array<double, 5> points{0, 2, 5, 8, 10};
+    std::array<double, 5> values{0, 4, 25, 64, 100};
+    CubicSpline<double, double, 4> s{points, values};
+    double answ = s.interpolate(1.);
+    ASSERT_NEAR(answ, 1., 10);
+
+    answ = s.interpolate(2.);
+    ASSERT_NEAR(answ, 4, 0.0000001);
+
+    answ = s.interpolate(3.);
+    ASSERT_NEAR(answ, 9, 10);
+
+    answ = s.interpolate(4.);
+    ASSERT_NEAR(answ, 16, 10);
+
+    answ = s.interpolate(5.);
+    ASSERT_NEAR(answ, 25, 10);
+
+    answ = s.interpolate(6.);
+    ASSERT_NEAR(answ, 36, 10);
+
+    answ = s.interpolate(7.);
+    ASSERT_NEAR(answ, 49, 10);
+}
+
+
+
+TEST(SplineTests, MainTest){
+    std::ofstream fileOut;
+    fileOut.open(py_path + "/Spline/err.txt");
+
+    std::array<double, 5> values5{};
+    std::array<double, 10> values10{};
+    std::array<double, 20> values20{};
+    std::array<double, 40> values40{};
+    std::array<double, 80> values80{};
+    std::array<double, 160> values160{};
+
+    std::array<double, 5> points5{};
+    std::array<double, 10> points10{};
+    std::array<double, 20> points20{};
+    std::array<double, 40> points40{};
+    std::array<double, 80> points80{};
+    std::array<double, 160> points160{};
+    constexpr std::array iter{5, 10, 20, 40, 80, 160};
+    double start = 0;
+    double end = 10;
+
+    double dx = (end - start)/double(iter[0]);
+    for(std::size_t j = 0; j < iter[0]; j++){
+        points5[j] = double(j) * dx;
+        values5[j] = std::exp(points5[j]);
+    }
+
+    dx = (end - start)/double(iter[1]);
+    for(std::size_t j = 0; j < iter[1]; j++){
+        points10[j] = double(j) * dx;
+        values10[j] = std::exp(points10[j]);
+    }
+
+    dx = (end - start)/double(iter[2]);
+    for(std::size_t j = 0; j < iter[2]; j++){
+        points20[j] = double(j) * dx;
+        values20[j] = std::exp(points20[j]);
+    }
+
+    dx = (end - start)/double(iter[3]);
+    for(std::size_t j = 0; j < iter[3]; j++){
+        points40[j] = double(j) * dx;
+        values40[j] = std::exp(points40[j]);
+    }
+
+    dx = (end - start)/double(iter[4]);
+    for(std::size_t j = 0; j < iter[4]; j++){
+        points80[j] = double(j) * dx;
+        values80[j] = std::exp(points80[j]);
+    }
+
+    dx = (end - start)/double(iter[5]);
+    for(std::size_t j = 0; j < iter[5]; j++){
+        points160[j] = double(j) * dx;
+        values160[j] = std::exp(points160[j]);
+    }
+    CubicSpline<double, double, 4> s5{points5, values5};
+    CubicSpline<double, double, 9> s10{points10, values10};
+    CubicSpline<double, double, 19> s20{points20, values20};
+    CubicSpline<double, double, 39> s40{points40, values40};
+    CubicSpline<double, double, 79> s80{points80, values80};
+    CubicSpline<double, double, 159> s160{points160, values160};
+
+    std::size_t N = 1000;
+    double maxx = -1.;
+    dx = (end - start)/double(N);
+    std::size_t it= 0;
+    for(std::size_t i = 0; i < N; i++){
+        if(std::abs(s5.interpolate(dx * double(i))- std::exp(dx * double(i))) > maxx){
+            maxx = std::abs(s5.interpolate(dx * double(i))- std::exp(dx * double(i)));
+            it = i;
+        }
+    }
+    
+    fileOut<<maxx<<"\n";
+
+    maxx = -1.;
+    for(std::size_t i = 0; i < N; i++){
+        if(std::abs(s10.interpolate(dx * double(i))- std::exp(dx * double(i))) > maxx){
+            maxx = std::abs(s10.interpolate(dx * double(i))- std::exp(dx * double(i)));
+            it = i;
+        }
+    }
+    
+    fileOut<<maxx<<"\n";
+
+    maxx = -1.;
+    for(std::size_t i = 0; i < N; i++){
+        if(std::abs(s20.interpolate(dx * double(i))- std::exp(dx * double(i))) > maxx){
+            maxx = std::abs(s20.interpolate(dx * double(i))- std::exp(dx * double(i)));
+            it = i;
+        }
+    }
+    
+    fileOut<<maxx<<"\n";
+
+    maxx = -1.;
+    for(std::size_t i = 0; i < N; i++){
+        if(std::abs(s40.interpolate(dx * double(i))- std::exp(dx * double(i))) > maxx){
+            maxx = std::abs(s40.interpolate(dx * double(i))- std::exp(dx * double(i)));
+            it = i;
+        }
+    }
+    
+
+    fileOut<<maxx<<"\n";
+
+    maxx = -1.;
+    for(std::size_t i = 0; i < N; i++){
+        if(std::abs(s80.interpolate(dx * double(i))- std::exp(dx * double(i))) > maxx){
+            maxx = std::abs(s80.interpolate(dx * double(i))- std::exp(dx * double(i)));
+            it = i;
+        }
+    }
+    
+
+    fileOut<<maxx<<"\n";
+
+    maxx = -1.;
+    for(std::size_t i = 0; i < N; i++){
+        if(std::abs(s160.interpolate(dx * double(i))- std::exp(dx * double(i))) > maxx){
+            maxx = std::abs(s160.interpolate(dx * double(i))- std::exp(dx * double(i)));
+            it = i;
+        }
+    }
+    
+
+    fileOut<<maxx<<"\n";
+    fileOut.close();
+}
